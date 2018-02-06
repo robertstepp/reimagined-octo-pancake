d7c9528d1507d4917b207061306609618e895224
Robert Stepp <robert@vyperinc.ninja>
2018-02-05 20:12:26 -0800

    Update

--- src/Robert.java
+++ src/Robert.java
@@ -8,20 +8,26 @@ import javax.swing.JTextField;
 
 public class Robert {
 	public static void main(String[] args) {
-		// http://www.java2s.com/Tutorials/Java/Swing_How_to/JOptionPane/Create_Multiple_input_in_JOptionPane_showInputDialog.htm
+		//
 		JTextField filename = new JTextField(10);
 		JTextField precinct = new JTextField(10);
+		JTextField date = new JTextField(10);
 		JPanel inputPanel = new JPanel();
 		inputPanel.add(new JLabel("Filename: (Case Sensitive)"));
 		inputPanel.add(filename);
 		inputPanel.add(Box.createHorizontalStrut(15)); // a spacer
 		inputPanel.add(new JLabel("Precinct:"));
 		inputPanel.add(precinct);
+		inputPanel.add(Box.createHorizontalStrut(15));
+		inputPanel.add(new JLabel("Dates requested (MM/DD/YYYY):"));
+		inputPanel.add(date);
 		int result = JOptionPane.showConfirmDialog(null, inputPanel,
-				"Please Enter X and Y Values", JOptionPane.OK_CANCEL_OPTION);
+				"Please Enter Filename, Precinct and Dates Requested",
+				JOptionPane.OK_CANCEL_OPTION);
 		if (result == JOptionPane.OK_OPTION) {
-			System.out.println("x value: " + filename.getText());
-			System.out.println("y value: " + precinct.getText());
+			System.out.println("Filename value: " + filename.getText());
+			System.out.println("Precinct value: " + precinct.getText());
+			System.out.println("Date value: " + date.getText());
 		}
 	}
 }
