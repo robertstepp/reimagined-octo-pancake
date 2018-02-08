d8561fab9feb4d5c67cea9e2f00f7ec48c2e171b
Ross <irvspe@gmail.com>
2018-02-08 13:59:17 -0800

    beat-map, modularizing user inputs

--- src/Ross.java
+++ src/Ross.java
@@ -6,7 +6,13 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+//import java.util.Arrays;
+import java.util.Collections;
+//import java.util.Date;
+//import java.util.List;
 
 import javax.swing.Box;
 import javax.swing.ImageIcon;
@@ -17,83 +23,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-//import java.util.Arrays;
-//import java.util.List;
-
 public class Ross {
-	public static String[] input() {
-		String[] types = { "Personal", "Property" };
-		String[] beats = { "B1", "B2", "B3", "C1", "C2", "C3", "D1", "D2", "D3",
-				"E1", "E2", "E3", "F1", "F2", "F3", "G1", "G2", "G3", "J1",
-				"J2", "J3", "K1", "K2", "K3", "L1", "L2", "L3", "M1", "M2",
-				"M3", "N1", "N2", "N3", "O1", "O2", "O3", "Q1", "Q2", "Q3",
-				"R1", "R2", "R3", "S1", "S2", "S3", "U1", "U2", "U3", "W1",
-				"W2", "W3" };
-		String[] precincts = { "N", "W", "E", "SE", "SW" };
-		String[] input = new String[5]; // Filename, Precinct, Beat, Date Range,
-										// Type (Personal/Property)
-		/*
-		 * Displays the Beat Map resized down.
-		 **/
-		JFrame frame = new JFrame();
-		ImageIcon icon = new ImageIcon(new ImageIcon("src/beat-map-2.png")
-				.getImage().getScaledInstance(647, 1000, Image.SCALE_SMOOTH));
-		JLabel label = new JLabel(icon);
-		frame.add(label);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
-		frame.setVisible(true);
-		JTextField filename = new JTextField(10);
-		JComboBox<?> precinct = new JComboBox<Object>(precincts);
-		JTextField startDate = new JTextField(10);
-		JTextField endDate = new JTextField(10);
-		JComboBox<?> beat = new JComboBox<Object>(beats);
-		JComboBox<?> type = new JComboBox<Object>(types);
-		JPanel inputFilename = new JPanel();
-		JPanel inputDate = new JPanel();
-		JPanel inputArea = new JPanel();
-		JPanel inputType = new JPanel();
-		inputFilename.add(new JLabel("Filename: (Case Sensitive)"));
-		inputFilename.add(filename);
-		inputDate.add(new JLabel("Start Date:"));
-		inputDate.add(startDate);
-		inputDate.add(Box.createHorizontalStrut(15));
-		inputDate.add(new JLabel("End Date:"));
-		inputDate.add(endDate);
-		inputArea.add(new JLabel("Precinct:"));
-		inputArea.add(precinct);
-		inputDate.add(Box.createHorizontalStrut(15));
-		inputArea.add(new JLabel("Beat:"));
-		inputArea.add(beat);
-		inputType.add(new JLabel("Type requested:"));
-		inputType.add(type);
-		JOptionPane.showConfirmDialog(null, inputFilename,
-				"Please Enter Filename:",
-				JOptionPane.OK_CANCEL_OPTION);
-		JOptionPane.showConfirmDialog(null, inputDate, "Enter requested dates:",
-				JOptionPane.OK_CANCEL_OPTION);
-		JOptionPane.showConfirmDialog(null, inputArea, "Enter requested area:",
-				JOptionPane.OK_CANCEL_OPTION);
-		JOptionPane.showConfirmDialog(null, inputType, "Enter requested type:",
-				JOptionPane.OK_CANCEL_OPTION);
-		/**
-		 * The following is how to return the data the user inputs I was testing
-		 * using an output to see the returned data.
-		 */
-		// System.out.println(types[type.getSelectedIndex()]);
-		// System.out.println("Filename value: " + filename.getText());
-		// System.out.printf("Start date: %s End date: %s\n",
-		// startDate.getText(),
-		// endDate.getText());
-		// System.out.printf("Precinct: %s Beat: %s\n",
-		// precincts[precinct.getSelectedIndex()],
-		// beats[beat.getSelectedIndex()]);
-		// System.out.println("Precinct value: " + precinct.getText());
-		// System.out.println("Date value: " + date.getText());
-
-		return input;
-	}
-
 	public static void printArray(ArrayList<String> al, String delim) {
 		for (String s : al)
 			System.out.print(s + delim);
@@ -116,8 +46,7 @@ public class Ross {
 	 * @param special
 	 *            Boolean value doesn't matter
 	 */
-	public static void printArray(ArrayList<String[]> outtie, String delim,
-			boolean special) {
+	public static void printArray(ArrayList<String[]> outtie, String delim, boolean special) {
 		// TODO: Either add ranges as a param, or remove "j" conditionals when
 		// done testing
 		int j = 0;
@@ -139,8 +68,7 @@ public class Ross {
 	 * @return A String ArrayList containing the column headings
 	 * @throws IOException
 	 */
-	public static String[] getColDefs(String file, String delim)
-			throws IOException {
+	public static String[] getColDefs(String file, String delim) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		// ArrayList<String> cols = new
 		// ArrayList<>(Arrays.asList(br.readLine().split(delim)));
@@ -159,8 +87,7 @@ public class Ross {
 	 * @throws IOException
 	 */
 	public static int numberOfRows(String file) throws IOException {
-		LineNumberReader lineReader = new LineNumberReader(
-				new FileReader(file));
+		LineNumberReader lineReader = new LineNumberReader(new FileReader(file));
 		lineReader.skip(Long.MAX_VALUE);
 		int numRows = lineReader.getLineNumber();
 		lineReader.close();
@@ -189,37 +116,199 @@ public class Ross {
 		return list;
 	}
 
-	public static ArrayList<String[]> recordsFromText(String file, String del,
-			int cols) throws IOException {
+	public static ArrayList<String[]> recordsFromText(String file, String del, String[] cols) throws IOException {
+		// The columnar positions of the fields we're recording unique or
+		// max/min values for
+		int bCol = -1, sCol = -1, pCol = -1, dCol = -1;
+		// Assumption: first column which contains keyword is the one we want
+		for (int c = 0; c < cols.length; ++c) {
+			String col = cols[c].toLowerCase();
+			if ((bCol == -1) && (col.contains("beat")))
+				bCol = c;
+			else if ((sCol == -1) && (col.contains("sector")))
+				sCol = c;
+			else if ((pCol == -1) && (col.contains("precinct")))
+				pCol = c;
+			else if ((dCol == -1) && (col.contains("date")))
+				dCol = c;
+		}
+
+		// For holding each unique value possible for the fields we'll present
+		// for user selection
+		ArrayList<String> beats = new ArrayList<String>(), sectors = new ArrayList<String>(),
+				precincts = new ArrayList<String>();
+		// https://docs.oracle.com/javase/tutorial/datetime/iso/format.html
+		// First actual date encountered will become the initial startDate and
+		// endDate
+		LocalDate startDate = LocalDate.MIN;
+		LocalDate endDate = LocalDate.MAX;
+		LocalDate currDate = LocalDate.MAX;
+
+		// Our precious records! (from every line after the header until null)
 		ArrayList<String[]> theRecords = new ArrayList<String[]>();
 		theRecords.ensureCapacity(numberOfRows(file));
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		// Ditch first line (presumed header)
 		br.readLine();
-		// TODO: make a different kinda loop? (avoid repeating curLine
-		// assignment)
+		// TODO: should this be a do-while loop? Not particularly important
 		String curLine = br.readLine();
 		String[] splitLine;
 		while (curLine != null) {
 			splitLine = listFromString(curLine, del);
-			if (splitLine.length == cols)
-				// System.out.print("~");
+			// Discard record if different number of columns than header
+			if (splitLine.length == cols.length) {
 				theRecords.add(splitLine);
+				// Add each unique occurrence of beats, sectors, and precincts
+				// to our lists of possibilities
+				if (!(beats.contains(splitLine[bCol])))
+					beats.add(splitLine[bCol]);
+				if (!(sectors.contains(splitLine[sCol])))
+					sectors.add(splitLine[sCol]);
+				if (!(precincts.contains(splitLine[pCol])))
+					precincts.add(splitLine[pCol]);
+				// Turn our date string into a LocalDate
+				// If the current record's date is earlier and/or later than our
+				// thus-far-seen extremes, it's the new extreme(s)
+				currDate = LocalDate.parse(splitLine[dCol], DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+				if (currDate.isBefore(startDate))
+					startDate = currDate;
+				if (currDate.isAfter(endDate))
+					endDate = currDate;
+			}
 			curLine = br.readLine();
 		}
 		br.close();
+
+		// Make our lists of possibilities "naturally ordered" (alphabetical)
+		Collections.sort(beats);
+		Collections.sort(sectors);
+		// We're preferring the order from the file
+		// Collections.sort(precincts);
+
+		// To group our date extremes
+		ArrayList<LocalDate> dateRange = new ArrayList<LocalDate>();
+		dateRange.add(startDate);
+		dateRange.add(endDate);
+
+		printArray(beats, ", ");
+		System.out.println(beats.size());
+		printArray(sectors, ", ");
+		System.out.println(sectors.size());
+		printArray(precincts, ", ");
+		System.out.println(precincts.size());
 		return theRecords;
+	}
+
+	public static void getChoices(ArrayList<LocalDate> limits, String imgLoc, ArrayList<String> beatOpts,
+			ArrayList<String> precOpts) {
+		getDateRange(limits);
+		displayMap(imgLoc);
+		getBeatPrecinct(beatOpts, precOpts);
+	}
+
+	public static void getDateRange(ArrayList<LocalDate> extremes) {
+
+	}
+
+	public static void displayMap(String mapLoc) {
+		/*
+		 * Displays the Beat Map resized down.
+		 **/
+		JFrame frame = new JFrame();
+		ImageIcon icon = new ImageIcon(
+				new ImageIcon(mapLoc).getImage().getScaledInstance(-1, 1000, Image.SCALE_SMOOTH));
+		JLabel label = new JLabel(icon);
+		frame.add(label);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void getBeatPrecinct(ArrayList<String> theBeats, ArrayList<String> thePrecincts) {
+
+	}
+
+	public static String[] input() {
+		String[] types = { "Personal", "Property" };
+		// displayMap("src/beat-map-2.png");
+		// return types;
+		String[] precincts = { "n", "s" };
+		String[] beats = { "q1", "r3" };
+
+		String[] input = new String[5]; // Filename, Precinct, Beat, Date Range,
+										// Type (Personal/Property)
+
+		JPanel inputFilename = new JPanel();
+		JTextField filename = new JTextField(10);
+		inputFilename.add(new JLabel("Filename: (Case Sensitive)"));
+		inputFilename.add(filename);
+		JOptionPane.showConfirmDialog(null, inputFilename, "Please Enter Filename:", JOptionPane.OK_CANCEL_OPTION);
+		System.out.println(filename.getText());
+
+		// Text boxes etc
+		JPanel inputDate = new JPanel();
+		JTextField startDate = new JTextField(10);
+		JTextField endDate = new JTextField(10);
+		inputDate.add(startDate);
+		inputDate.add(Box.createHorizontalStrut(15));
+		inputDate.add(new JLabel("End Date:"));
+		inputDate.add(endDate);
+		
+		JPanel inputArea = new JPanel();
+		JComboBox<?> precinct = new JComboBox<Object>(precincts);
+		JComboBox<?> beat = new JComboBox<Object>(beats);
+
+		JPanel inputType = new JPanel();
+		JComboBox<?> type = new JComboBox<Object>(types);
+
+		// Applying names etc inputDate.add(new JLabel("Start Date:"));
+		inputArea.add(new JLabel("Precinct:"));
+		inputArea.add(precinct);
+
+		inputDate.add(Box.createHorizontalStrut(15));
+
+		inputArea.add(new JLabel("Beat:"));
+		inputArea.add(beat);
+
+		inputType.add(new JLabel("Type requested:"));
+		inputType.add(type);
+
+		JOptionPane.showConfirmDialog(null, inputDate, "Enter requested dates:", JOptionPane.OK_CANCEL_OPTION);
+		JOptionPane.showConfirmDialog(null, inputArea, "Enter requested area:", JOptionPane.OK_CANCEL_OPTION);
+		JOptionPane.showConfirmDialog(null, inputType, "Enter requested type:",
+				JOptionPane.OK_CANCEL_OPTION); /*
+												 * The following is how to
+												 * return the data the user
+												 * inputs I was testing using an
+												 * output to see the returned
+												 * data.
+												 */
+
+		// System.out.println(types[type.getSelectedIndex()]);
+		// System.out.println("Filename value: " + filename.getText());
+		// System.out.printf("Start date: %s End date: %s\n",
+		// startDate.getText(),
+		// endDate.getText());
+		// System.out.printf("Precinct: %s Beat: %s\n",
+		// precincts[precinct.getSelectedIndex()],
+		// beats[beat.getSelectedIndex()]);
+		// System.out.println("Precinct value: " + precinct.getText());
+		// System.out.println("Date value: " + date.getText());
+
+		return input;
 	}
 
 	public static void main(String[] args) throws IOException {
 		final boolean debug = true;
 		String filename = "datasets/original raw data-DON'T MODIFY.csv";
+		String mapLoc = "src/beat-map-2.png";
 		String delimiter = ",";
 		input();
+		ArrayList<String> a = new ArrayList<String>();
+		// getChoiceFromOpts(a, a, a, mapLoc);
 
 		String[] columns = getColDefs(filename, delimiter);
-		ArrayList<String[]> rows = new ArrayList<String[]>(
-				recordsFromText(filename, delimiter, columns.length));
+		ArrayList<String[]> rows = new ArrayList<String[]>(recordsFromText(filename, delimiter, columns));
 
 		if (debug) {
 			printArray(rows, " \t ", true);
